676bb407b0bcb81d980297289e2ac8e96e3be548
Spyke <vidal.quentin@pog-tv.com>
2024-12-11 17:43:02 +0100

    initial commit. Working overlay 1

---
+++ POGMonitorOverlay/src/preload.ts
@@ -0,0 +1,27 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { ipcRenderer, contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+    setBackground: (callback: (imageData: any) => void) => {
+        ipcRenderer.on('set-background', (event: any, imageData: any) => {
+            console.log('set-background', imageData);
+            callback(imageData);
+        });
+    },
+    getSources: async () => {
+        return await ipcRenderer.invoke('getSources');
+    },
+    getOperatingSystem: async () => {
+        return await ipcRenderer.invoke('getOperatingSystem');
+    },
+    toggleMouseEvent: async (value: boolean) => {
+        if (value) {
+            ipcRenderer.send('mouse-events-enable');
+        } else {
+            ipcRenderer.send('mouse-events-disable');
+        }
+    }
+});
+
